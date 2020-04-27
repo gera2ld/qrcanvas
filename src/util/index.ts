cbd3cfc3134df2a92fd2f105e8087049bc46a199
Gerald <gera2ld@163.com>
2020-04-27 17:33:19 +0800

    fix: ts

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,10 +1,10 @@
 import helpers from './helpers';
 
 export { helpers };
-export effects from './effects';
+export { default as effects } from './effects';
 
 export function setCanvasModule(canvasModule) {
-  const { Canvas, Image, createCanvas } = createCanvas;
+  const { Canvas, Image, createCanvas } = canvasModule;
   const isCanvas = el => el instanceof Canvas;
   const isDrawable = el => isCanvas(el) || el instanceof Image;
   helpers.createCanvas = createCanvas;
